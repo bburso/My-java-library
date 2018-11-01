460321c96d521cf0c9aa70f445d2d96dd094cf0b
Bryn Burson <bburso@users.noreply.github.com>
2018-11-01 13:33:39 +0100

    some updates

---
+++ BBStringOutput.java
@@ -0,0 +1,19 @@
+
+
+public class StringOutput
+{
+	void print(String s)  {
+		System.out.println(s);		
+	}
+	
+	String convertToString(int i)  {
+		String str = String.valueOf(i);
+
+		return str;
+	}
+	void printint(int i)  {
+		System.out.println(i);		
+	}
+	
+	
+}
--- MainApp.java
+++ MainApp.java
@@ -1,11 +1,11 @@
 
-package MainPackage;
+
 /**
  *Main app file
  *
  */
 
-public class MainApp 
+public class MainApp
 
 {
 	
@@ -14,14 +14,13 @@ public static void main(String[] args)
 {
                
  StringOutput so = new StringOutput();
+
  
  
      
-   so.print("test out");
-   so.print("numbers:");
+   so.print("testnnnn out");
+   so.print("numbbbbers:");
    
  }
-	
-	
 
 }
